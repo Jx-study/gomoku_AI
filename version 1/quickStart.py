f4feb7f8c9472d5bc5165e9e110e3fb707120093
JX_Tee <145241060+Jx-study@users.noreply.github.com>
2024-07-18 15:24:45 +0800

    Update quickStart.py

--- version 1/quickStart.py
+++ version 1/quickStart.py
@@ -15,14 +15,14 @@ initFile('B.txt', 'R')
 def compileC(source_file, output_file):
     subprocess.run(['gcc', source_file, '-o', output_file], check=True)
 
-compileC('playerA_test.c', 'playerA_test')
+compileC('playerA.c', 'playerA')
 compileC('playerB.c', 'playerB')
 
 chessProcess = subprocess.Popen(['python', 'chess.py'], stdin=subprocess.PIPE)
 chessProcess.stdin.write(b'A.txt\nB.txt\n')
 chessProcess.stdin.close()
 
-playerAProcess = subprocess.Popen(['./playerA_test'], stdin=subprocess.PIPE)
+playerAProcess = subprocess.Popen(['./playerA'], stdin=subprocess.PIPE)
 playerAProcess.stdin.write(b'A.txt\nA\n')
 playerAProcess.stdin.close()
 
